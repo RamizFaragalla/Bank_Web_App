2d54ad5bd6833b75e61004941c162b58197ffcc1
RamizFaragalla <ramezkelada66@gmail.com>
2021-11-04 16:44:50 -0400

    changes to index.js

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 
 // Load each controller
-const postsController = require('./posts.js');
 const appConfigController = require('./appConfig.js');
 const loginController = require('./login.js');
 const accountsController = require('./accounts.js');
@@ -11,7 +10,6 @@ const transactionsController = require('./transactions.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
-router.use('/posts', postsController);
 router.use('/application-configuration', appConfigController);
 router.use('/login', loginController);
 router.use('/accounts', accountsController);
