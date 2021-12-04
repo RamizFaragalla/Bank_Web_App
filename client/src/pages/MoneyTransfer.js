db615b41767a773e60bfc73291d7197fd16ea282
RamizFaragalla <ramezkelada66@gmail.com>
2021-12-04 12:40:05 -0500

    add CSS to MoneyTransfer.js

--- client/src/pages/MoneyTransfer.js
+++ client/src/pages/MoneyTransfer.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import Loading from '../components/Loading';
 
+const appStyle = {
+	height: '250px',
+  display: 'flex'
+};
+
+const formStyle = {
+  margin: 'auto',
+  padding: '10px',
+  border: '1px solid #c9c9c9',
+  borderRadius: '5px',
+  background: '#f5f5f5',
+  width: '220px',
+  display: 'block'
+};
+
+const labelStyle = {
+  margin: '10px 0 5px 0',
+  fontFamily: 'Arial, Helvetica, sans-serif',
+  fontSize: '15px',
+};
+
+const inputStyle = {
+  margin: '5px 0 10px 0',
+  padding: '5px', 
+  border: '1px solid #bfbfbf',
+  borderRadius: '3px',
+  boxSizing: 'border-box',
+  width: '100%'
+};
+
+const submitStyle = {
+  margin: '10px 0 0 0',
+  padding: '7px 10px',
+  border: '1px solid #efffff',
+  borderRadius: '3px',
+  background: '#3085d6',
+  width: '100%', 
+  fontSize: '15px',
+  color: 'white',
+  display: 'block'
+};
+
 const Field = React.forwardRef(({label, type}, ref) => {
     return (
     <div>
-        <label>{label}</label>
-        <input ref={ref} type={type}/>
+        <label style={labelStyle} >{label}</label>
+        <input ref={ref} type={type} style={inputStyle} />
     </div>
     );
 });
@@ -25,12 +67,12 @@ const Form = ({onSubmit}) => {
       onSubmit(data);
   };
   return (
-    <form onSubmit={handleSubmit} >
+    <form style={formStyle} onSubmit={handleSubmit} >
       <Field ref={recipient_account_num_ref} label="recipient_account_num:" type="text" />
       <Field ref={amount_ref} label="amount:" type="text" />
       <Field ref={description_ref} label="description:" type="text" />
       <div>
-        <button type="submit">Transfer Money</button>
+        <button style={submitStyle} type="submit">Transfer Money</button>
       </div>
     </form>
   );
@@ -95,16 +137,16 @@ class MoneyTransfer extends React.Component {
     return (
         <div>
           <div>
-            money_transfer
+            Money Transfer
           </div>
 
           <br/>
 
-          <div>
+          <div style={appStyle}>
           <Form onSubmit={this.handleSubmit} />
           </div>
 
-          <br/>
+          <br/><br/><br/><br/>
 
           <div>
             {this.state.output}
